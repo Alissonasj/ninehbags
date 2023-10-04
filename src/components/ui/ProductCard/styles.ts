5944007b8da05787cbd6a012af55024b1da66e3b
Alisson Silva <30477408+Alissonasj@users.noreply.github.com>
2023-10-04 18:00:40 +0000

    coding

--- src/components/ui/ProductCard/styles.ts
+++ src/components/ui/ProductCard/styles.ts
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    
+`;
 
 export const ProductLink = styled.a``;
 
 export const ProductImg = styled.img`
     max-width: 460px;
+    margin-bottom: 1rem;
+    border-radius: 0 1.25rem 0;
 `;
 
-export const ProductName = styled.h3``;
+export const ProductName = styled.h3`
+    margin-bottom: 1.5rem;
+`;
 
 export const ProductPrice = styled.span``;
--- src/components/ui/ProductSection/styles.ts
+++ src/components/ui/ProductSection/styles.ts
@@ -5,8 +5,13 @@ export const ProductSection = styled.section`
 
     margin-top: 6.25rem;
     margin-inline: 13.125rem;
-
+    display: grid;
+    grid-column-gap: 1.25rem;
+    grid-row-gap: 2.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 460px));
     & > nav {
         margin-bottom: 2.5rem;
     }
 `;
+
+export const GridProductSection = styled.div
