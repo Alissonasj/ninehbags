1eee0e0fc3ed5d2514e0094e64b907b243b59a16
Alisson Silva <30477408+Alissonasj@users.noreply.github.com>
2023-10-05 15:47:17 -0300

    add fetch

--- src/components/ui/ProductSection/index.tsx
+++ src/components/ui/ProductSection/index.tsx
@@ -1,15 +1,29 @@
-import product from "@assets/imgs/product.jpg";
 import MenuNav from "@ui/MenuNav";
 import ProductCard from "@ui/ProductCard";
+import { useEffect, useState } from "react";
 import * as S from "./styles";
 
 const ProductSection = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("http://localhost:3000/products");
+            console.log(response.ok);
+
+            const data = await response.json();
+            console.log(data);
+
+            return setProducts(data);
+        })();
+    }, []);
+
     return (
         <S.ProductSection>
             <MenuNav />
 
             <S.GridProductSection>
-                <ProductCard
+                {/* <ProductCard
                     srcImg={product}
                     product={{ name: "Moon Bag", price: "R$99,00" }}
                 />
@@ -40,7 +54,19 @@ const ProductSection = () => {
                 <ProductCard
                     srcImg={product}
                     product={{ name: "Moon Bag", price: "R$99,00" }}
-                />
+                /> */}
+                {products.map((product) => {
+                    return (
+                        <ProductCard
+                            key={product.id}
+                            srcImg={product.img}
+                            product={{
+                                name: product.name,
+                                price: product.price,
+                            }}
+                        />
+                    );
+                })}
             </S.GridProductSection>
         </S.ProductSection>
     );
