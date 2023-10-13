f033e613a3fccac65739536a853bb05ca00f9d07
Alissonasj <alissonsilva.jesus@gmail.com>
2023-10-13 11:46:42 -0300

    product page start

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
+import Home from "@pages/home";
 import { Grid } from "@styles/Grid";
-import Banner from "@ui/Banner";
 import Footer from "@ui/Footer";
 import Header from "@ui/Header";
-import ProductSection from "@ui/ProductSection";
 
 function App() {
     return (
-        <Grid>
-            <Header />
-            <Banner />
-            <ProductSection />
-            <Footer />
-        </Grid>
+        <>
+            <Grid>
+                <Header />
+                <Home />
+                <Footer />
+            </Grid>
+        </>
     );
 }
 
